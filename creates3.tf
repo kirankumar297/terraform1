ba70fcd8d808f80d446485bc5879a78f0b1c7f16
kirankumar297 <58037059+kirankumar297@users.noreply.github.com>
2020-01-14 00:34:12 +0530

    Update creates3.tf

--- creates3.tf
+++ creates3.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIA4YJ6WJZAULMRK6E3"
-  secret_key = "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"
-
+  #region = "ap-south-1"
+  #access_key = "AKIA4YJ6WJZAULMRK6E3"
+  #secret_key = "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"
+assume_role {
+    role_arn     = "arn:aws:iam::876841684545:role/createS3"
+    #session_name = "SESSION_NAME"
+    #external_id  = "EXTERNAL_ID"
+  }
 }
 
 resource "aws_s3_bucket" "b" {
