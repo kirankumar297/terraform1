be419c8a5c6a1168ecac5649637f7374b803cf26
kirankumar297 <58037059+kirankumar297@users.noreply.github.com>
2020-01-14 00:37:20 +0530

    Update creates3.tf

--- creates3.tf
+++ creates3.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  #region = "ap-south-1"
+  region = "ap-south-1"
   #access_key = "AKIA4YJ6WJZAULMRK6E3"
   #secret_key = "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"
 assume_role {
